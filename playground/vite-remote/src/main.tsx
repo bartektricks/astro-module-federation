6098f32c1299f1da6da3619f90f526601b4e35cd
Bartlomiej Ciszewski <bartektricks@gmail.com>
2025-05-09 13:03:05 +0200

    chore: migrate from eslint to biome

--- playground/vite-remote/src/main.tsx
+++ playground/vite-remote/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
